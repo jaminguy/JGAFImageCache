dc1b0426dcad202adad76a1935e78b8a239bfbd6
Jamin Guy <jaminguy@gmail.com>
2013-10-03 14:23:48 -0500

    Adds CC_LONG cast for 64 bit support

--- NSString+JGAFSHA1.m
+++ NSString+JGAFSHA1.m
@@ -16,7 +16,7 @@
     const char *cstr = [self cStringUsingEncoding:NSUTF8StringEncoding];
     NSData *data = [NSData dataWithBytes:cstr length:self.length];
     uint8_t digest[CC_SHA1_DIGEST_LENGTH];
-    CC_SHA1(data.bytes, data.length, digest);
+    CC_SHA1(data.bytes, (CC_LONG)data.length, digest);
     NSMutableString* output = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 2];
     for(int i = 0; i < CC_SHA1_DIGEST_LENGTH; i++) {
         [output appendFormat:@"%02x", digest[i]];
